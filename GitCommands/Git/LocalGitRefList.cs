5a38c6a1809493809dbb30f93305b05f77ea973b
Szali Szali <szalicil@gmail.com>
2018-04-24 22:59:35 +0200

    ugly

--- GitCommands/Git/LocalGitRefList.cs
+++ GitCommands/Git/LocalGitRefList.cs
@@ -14,51 +14,18 @@ namespace GitCommands.Git
     /// </summary>
     public sealed class LocalGitRefList
     {
-        [NotNull, ItemNotNull]
-        private readonly Item[] _items;
+        [NotNull, ItemNotNull] private readonly Item[] _items;
 
-        public LocalGitRefList([NotNull]GitCommandResult result)
+        public LocalGitRefList([NotNull] GitCommandResult result)
         {
             _items = result.Lines.Select(l => new Item(l)).ToArray();
         }
 
         public IReadOnlyList<IGitRef> AllGitRefs => new ReadOnlyCollection<IGitRef>(_items.Select(i => i.Ref).ToList());
 
-        [NotNull, ItemNotNull]
-        public IReadOnlyList<IGitRef> OrderedBranches(BranchOrdering ordering)
-        {
-            return OrderedFilteredGitRefs(ordering, i => i.Ref.IsHead);
-        }
-
-        [NotNull, ItemNotNull]
-        public IReadOnlyList<IGitRef> OrderedTags(BranchOrdering ordering)
-        {
-            return OrderedFilteredGitRefs(ordering, i => i.Ref.IsTag);
-        }
-
-        [NotNull, ItemNotNull]
-        private IReadOnlyList<IGitRef> OrderedFilteredGitRefs(BranchOrdering ordering, [NotNull] Func<Item, bool> filter)
-        {
-            return SortFunction(ordering)(_items.Where(filter)).Select(i => i.Ref).ToArray();
-        }
-
-        [NotNull]
-        private Func<IEnumerable<Item>, IEnumerable<Item>> SortFunction(BranchOrdering ordering)
-        {
-            switch (ordering)
-            {
-                case BranchOrdering.ByLastAccessDate:
-                    return list => list.OrderByDescending(item => item.Date);
-                case BranchOrdering.Alphabetically:
-                    return list => list.OrderBy(item => item.Ref.CompleteName);
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(ordering), ordering, null);
-            }
-        }
-
         private sealed class Item
         {
-            public Item([NotNull]string line)
+            public Item([NotNull] string line)
             {
                 var columns = line.Split(new[] { ' ' }, 4);
 
@@ -70,7 +37,51 @@ namespace GitCommands.Git
             }
 
             public DateTime Date { get; }
-            [NotNull] public IGitRef Ref { get; }
+
+            [NotNull]
+            public IGitRef Ref { get; }
+        }
+
+        public sealed class Sorted
+        {
+            [NotNull] private readonly LocalGitRefList _unsortedList;
+
+            public Sorted([NotNull] GitCommandResult result)
+            {
+                _unsortedList = new LocalGitRefList(result);
+            }
+
+            [NotNull, ItemNotNull]
+            public IReadOnlyList<IGitRef> OrderedBranches(BranchOrdering ordering)
+            {
+                return OrderedFilteredGitRefs(ordering, i => i.Ref.IsHead);
+            }
+
+            [NotNull, ItemNotNull]
+            public IReadOnlyList<IGitRef> OrderedTags(BranchOrdering ordering)
+            {
+                return OrderedFilteredGitRefs(ordering, i => i.Ref.IsTag);
+            }
+
+            [NotNull, ItemNotNull]
+            private IReadOnlyList<IGitRef> OrderedFilteredGitRefs(BranchOrdering ordering, [NotNull] Func<Item, bool> filter)
+            {
+                return SortFunction(ordering)(_unsortedList._items.Where(filter)).Select(i => i.Ref).ToArray();
+            }
+
+            [NotNull]
+            private Func<IEnumerable<Item>, IEnumerable<Item>> SortFunction(BranchOrdering ordering)
+            {
+                switch (ordering)
+                {
+                    case BranchOrdering.ByLastAccessDate:
+                        return list => list.OrderByDescending(item => item.Date);
+                    case BranchOrdering.Alphabetically:
+                        return list => list.OrderBy(item => item.Ref.CompleteName);
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(ordering), ordering, null);
+                }
+            }
         }
     }
 }
